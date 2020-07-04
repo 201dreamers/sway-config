091b522894664a2e7659bc2fbbdc411208d5b3ff
201dreamers <201dreamers@gmail.com>
2020-07-05 00:59:46 +0300

    Written script for playinf sound when battery is critical and waybar style changes

---
+++ .config/sway/modules/critical_battery_beeper/critical_battery_beeper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+from time import sleep
+
+
+def get_battery_status():
+    return open('/sys/class/power_supply/BAT0/status', 'r').readline().strip()
+
+
+def get_battery_capacity():
+    return int(open('/sys/class/power_supply/BAT0/capacity', 'r').readline().strip())
+
+
+while True:
+    if get_battery_status() == "Discharging":
+        battery_capacity = get_battery_capacity()
+        if battery_capacity <= 5 or battery_capacity == 10:
+            os.system('mpg123 -q ~/.config/sway/modules/critical_battery_beeper/critical_battery.mp3')
+    sleep(40)
