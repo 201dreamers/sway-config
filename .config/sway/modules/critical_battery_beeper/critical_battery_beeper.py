93b70e7280bfcfca663e100cd108743989e7b5b8
201dreamers <201dreamers@gmail.com>
2020-07-16 14:32:50 +0300

    eyecandy changes

--- .config/sway/modules/critical_battery_beeper/critical_battery_beeper.py
+++ .config/sway/modules/critical_battery_beeper/critical_battery_beeper.py
@@ -15,6 +15,6 @@ def get_battery_capacity():
 while True:
     if get_battery_status() == "Discharging":
         battery_capacity = get_battery_capacity()
-        if battery_capacity <= 5 or battery_capacity == 10:
+        if battery_capacity <= 10:
             os.system('mpg123 -q ~/.config/sway/modules/critical_battery_beeper/critical_battery.mp3')
-    sleep(40)
+    sleep(60)
